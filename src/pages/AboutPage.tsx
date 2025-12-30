1b6494c140eb6d383406d9c96f637bf16733e68f
BadisBenidir <107676252+BadisBenidir@users.noreply.github.com>
2025-12-30 01:27:34 +0000

    photos

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -131,7 +131,7 @@ export default function AboutPage() {
             
             <div className="relative order-1 lg:order-2">
               <img
-                src="/photos-site/DSC00325_ARW.jpeg"
+                src="/photos-site/sac_balenciaga.jpg"
                 alt="Maroquinerie de luxe"
                 className="w-full h-64 sm:h-80 md:h-96 object-cover rounded-2xl shadow-2xl"
               />
