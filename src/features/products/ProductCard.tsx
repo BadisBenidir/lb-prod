1beea22461146f600d1b6083c8366b0c3b83f18a
BadisBenidir <107676252+BadisBenidir@users.noreply.github.com>
2026-01-08 23:59:37 +0000

    m

--- src/features/products/ProductCard.tsx
+++ src/features/products/ProductCard.tsx
@@ -26,8 +26,8 @@ const ProductCard: React.FC<ProductCardProps> = ({ product, onAddToCart, onProdu
           className="w-full h-64 md:h-80 object-contain group-hover:scale-105 transition-transform duration-500"
         />
         {product.isSoldDisplay && (
-          <div className="absolute top-2 left-2 z-10">
-            <span className="bg-red-100 text-red-700 text-xs font-semibold px-2 py-1 rounded-md uppercase tracking-wide">VENDU</span>
+          <div className="absolute inset-0 bg-black bg-opacity-50 top-2 left-2 z-10">
+            <span className="bg-red-100 text-red-700 text-m font-semibold px-2 py-1 rounded-md uppercase tracking-wide">VENDU</span>
           </div>
         )}
         {!product.inStock && !product.isSoldDisplay && (
