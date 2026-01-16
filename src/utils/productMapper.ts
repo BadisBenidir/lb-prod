50c8560c63adc3450b2b3372b65f4fa658f0fe7b
BadisBenidir <107676252+BadisBenidir@users.noreply.github.com>
2026-01-16 21:50:06 +0000

    Modifications 16/01/2026 V3

--- src/utils/productMapper.ts
+++ src/utils/productMapper.ts
@@ -5,11 +5,11 @@ import { DbProductWithRelations, Product } from '../types';
  */
 export const mapConditionToClient = (dbCondition: string): Product['condition'] => {
   const conditionMap: Record<string, Product['condition']> = {
-    'New': 'New',
-    'Excellent': 'Excellent',
-    'Very Good': 'Very Good',
-    'Good': 'Good',
-    'Fair': 'Fair'
+    'new': 'New',
+    'excellent': 'Excellent',
+    'very good': 'Very Good',
+    'good': 'Good',
+    'fair': 'Fair'
   };
   
   return conditionMap[dbCondition] || 'Good';
