a81e355810cc45324d69ac4f4f680a308617d5e0
BadisBenidir <107676252+BadisBenidir@users.noreply.github.com>
2026-01-20 00:54:32 +0000

    Modifications 20/01/2026 V1

--- src/pages/HomePageDesktop.tsx
+++ src/pages/HomePageDesktop.tsx
@@ -5,6 +5,7 @@ import { ProductGrid } from '../features/products';
 import { useProducts, useAvailableCategories } from '../hooks/useProducts';
 import { useCartContext } from '../contexts/CartContext';
 import { useAuth } from '../contexts/AuthContext';
+import {HeroDesktop} from '../components/common/HeroDesktop';
 
 const HomePageDesktop: React.FC = () => {
   const navigate = useNavigate();
@@ -33,12 +34,25 @@ const HomePageDesktop: React.FC = () => {
     window.scrollTo(0, 0);
   };
 
+  const heroProducts = products
+    .filter((p) => heroIds.includes(p.id))
+    .sort((a, b) => heroIds.indexOf(a.id) - heroIds.indexOf(b.id));
+
+
+  const heroIds = [
+    "e2a0a467-21af-4747-8563-37a4a0fa7ba9";
+    "413c5c99-60d3-40e8-9fa8-4f8c47b0724c";
+    "4e4990d3-217f-40f5-942b-c2dfe6d417b5";
+    "d7c2d6b4-aa3b-429a-a4c2-6cbe85297d0b";
+  ];
+
+
   return (
     <main>
       <Hero onNavigate={handleNavigation} />
       <BoutiqueHighlight 
         onNavigateToBoutique={() => navigate('/boutique')}
-        featuredProducts={products.slice(0, 4)}
+        featuredProducts={heroProducts}
       />
       <ProductGrid 
         products={products}
