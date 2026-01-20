75f8019a347a2740abe175ca54ead87ea5c9b88d
BadisBenidir <107676252+BadisBenidir@users.noreply.github.com>
2026-01-20 22:38:52 +0000

    Modifications 20/01/2026 V1

--- src/features/products/ProductGrid.tsx
+++ src/features/products/ProductGrid.tsx
@@ -24,6 +24,13 @@ interface ProductGridProps {
     "8ce7f2b7-a537-4cf3-944f-3f5dfba61128",
   ];
 
+  const boutiqueHighlightIds = [
+    "413c5c99-60d3-40e8-9fa8-4f8c47b0724c",
+    "e2a0a467-21af-4747-8563-37a4a0fa7ba9",
+    "4e4990d3-217f-40f5-942b-c2dfe6d417b5",
+    "d7c2d6b4-aa3b-429a-a4c2-6cbe85297d0b",
+  ];
+
 
 const ProductGrid: React.FC<ProductGridProps> = ({ products, onAddToCart, categories, userId, homeToutIds = [] }) => {
   const navigate = useNavigate();
