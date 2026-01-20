201edc429d0d7df6f5022f7213dad3e08f9175e4
BadisBenidir <107676252+BadisBenidir@users.noreply.github.com>
2026-01-20 01:35:35 +0000

    Modifications 20/01/2026 V1

--- src/pages/HomePageDesktop.tsx
+++ src/pages/HomePageDesktop.tsx
@@ -34,19 +34,6 @@ const HomePageDesktop: React.FC = () => {
     window.scrollTo(0, 0);
   };
 
-  const heroProducts = products
-    .filter((p) => heroIds.includes(p.id))
-    .sort((a, b) => heroIds.indexOf(a.id) - heroIds.indexOf(b.id));
-
-
-  const heroIds = [
-    "e2a0a467-21af-4747-8563-37a4a0fa7ba9",
-    "413c5c99-60d3-40e8-9fa8-4f8c47b0724c",
-    "4e4990d3-217f-40f5-942b-c2dfe6d417b5",
-    "d7c2d6b4-aa3b-429a-a4c2-6cbe85297d0b",
-  ];
-
-
   return (
     <main>
       <Hero onNavigate={handleNavigation} />
