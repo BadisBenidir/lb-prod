810f7f6e88943fc42319b4abdec673309b44f88a
BadisBenidir <107676252+BadisBenidir@users.noreply.github.com>
2026-01-22 00:33:48 +0000

    Modifications 21/01/2026 V1

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,23 +1,21 @@
-import React, { useEffect } from "react";
+import React { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
-type Props = { children: React.ReactNode };
+type Props = {
+    children: React.ReactNode;
+};
 
-export default function ScrollToTop({ children }: { children: React.ReactNode}) {
-    const { pathname } = useLocation();
+export default function ScrollToTop() {
+    const { pathname, search } = useLocation();
 
     useEffect(() => {
-        window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
-
         const el = document.getElementById('app-scroll');
-        el?.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+        if (el) {
+            el.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+        } else {
+            window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+        }
     }, [pathname]);
 
-    useEffect(() => {
-        if ('scrollRestoration' in window.history) {
-            window.history.scrollRestoration = 'manual';
-        }
-    }, []);
-
-    return <>{children}</>;
+    return null;
 }
