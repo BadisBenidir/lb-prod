30a02bae7b150e9399af2424329acd16b3eccd7f
BadisBenidir <107676252+BadisBenidir@users.noreply.github.com>
2026-01-22 02:08:12 +0000

    Modifications 21/01/2026 V1

--- src/components/Boutique/FilterSidebarMobile.tsx
+++ src/components/Boutique/FilterSidebarMobile.tsx
@@ -242,7 +242,7 @@ const FilterSidebarMobile: React.FC<FilterSidebarMobileProps> = ({
         </div>
 
         {/* Content */}
-        <div className="h-full overflow-y-auto pb-28">
+        <div className="h-full overflow-y-auto pb-[120px]">
           {renderCheckboxSection('Catégories', 'categories', 'categories', availableCategories)}
           {renderCheckboxSection('Marques', 'brands', 'brands', availableBrands)}
           {renderCheckboxSection('Couleurs', 'colors', 'colors', availableColors)}
