f3bbbd7b837a1dd4d297f872603978d2e505b0a3
BadisBenidir <107676252+BadisBenidir@users.noreply.github.com>
2026-01-24 20:36:51 +0000

    Modifications 22/01/2026 V1

--- src/components/Boutique/FilterSidebar.tsx
+++ src/components/Boutique/FilterSidebar.tsx
@@ -261,7 +261,9 @@ const FilterSidebar: React.FC<FilterSidebarProps> = ({
 
                 {otherBrands.length > 0 && (
                   <>
-                  <div className='my-3 border-t border-gray-900' />
+                  <div className='my-6 flex justify-center'>
+                    <div className='w-1/3 border-t border-gray-500' />
+                  </div>
                   {otherBrands.map((brand: string) => (
                     <label key={brand} className="flex items-center">
                       <input
