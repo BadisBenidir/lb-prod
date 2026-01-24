c0984de9bf3f4d3128d4a1c1c9d57a198661d931
BadisBenidir <107676252+BadisBenidir@users.noreply.github.com>
2026-01-24 21:06:33 +0000

    Modifications 22/01/2026 V1

--- src/components/Boutique/FilterSidebar.tsx
+++ src/components/Boutique/FilterSidebar.tsx
@@ -247,7 +247,7 @@ const FilterSidebar: React.FC<FilterSidebarProps> = ({
             </button>
             {expandedSections.brands && (
               <div className="space-y-2 max-h-40 md:max-h-48 overflow-y-auto">
-                <div className='space-y-3'>
+                <div className='space-y-2'>
                   {mainBrands.map((brand: string) => (
                     <label key={brand} className="flex items-center">
                       <input
@@ -267,7 +267,7 @@ const FilterSidebar: React.FC<FilterSidebarProps> = ({
                   </div>
                 )}
 
-                <div className='space-y-3'>
+                <div className='space-y-2'>
                   {otherBrands.map((brand: string) => (
                     <label key={brand} className="flex items-center">
                       <input
