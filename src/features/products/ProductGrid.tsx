e2f27507e2840a9d7178dabc98780a10f73b67a9
BadisBenidir <107676252+BadisBenidir@users.noreply.github.com>
2026-01-25 22:24:17 +0000

    Modifications 22/01/2026 V1

--- src/features/products/ProductGrid.tsx
+++ src/features/products/ProductGrid.tsx
@@ -121,7 +121,7 @@ const ProductGrid: React.FC<ProductGridProps> = ({ products, onAddToCart, catego
         ))}
       </div>
       
-      <div className='grid grid-cols-2 gap-4 md:grid-cols-3'>
+      <div className='mx-auto max-w-5xl grid grid-cols-2 gap-4'>
         {productsToShow.map((product) => (
           <div key={product.id}>
             {/* Mobile */}
