dc5d6453abc2cea87797b601bdf873dfc31085d2
BadisBenidir <107676252+BadisBenidir@users.noreply.github.com>
2026-01-25 22:20:33 +0000

    Modifications 22/01/2026 V1

--- src/components/common/highlight.ts
+++ src/components/common/highlight.ts
@@ -1,6 +1,6 @@
 export const BOUTIQUE_HIGHLIGHT_IDS = [
   "1e42624b-0bdc-47a8-acc4-746d6e212134",
   "413c5c99-60d3-40e8-9fa8-4f8c47b0724c",
-  "e2a0a467-21af-4747-8563-37a4a0fa7ba9",
   "d7c2d6b4-aa3b-429a-a4c2-6cbe85297d0b",
+  "e2a0a467-21af-4747-8563-37a4a0fa7ba9",
 ];
