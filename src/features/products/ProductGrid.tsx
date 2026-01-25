9bdca52ca463bd27c44132bfd5075c5641d1a299
BadisBenidir <107676252+BadisBenidir@users.noreply.github.com>
2026-01-25 21:06:32 +0000

    Modifications 22/01/2026 V1

--- src/features/products/ProductGrid.tsx
+++ src/features/products/ProductGrid.tsx
@@ -25,9 +25,9 @@ interface ProductGridProps {
 ];
 
 const boutiqueHighlightIds = [
-  "413c5c99-60d3-40e8-9fa8-4f8c47b0724c",
+  "1e42624b-0bdc-47a8-acc4-746d6e212134",
   "e2a0a467-21af-4747-8563-37a4a0fa7ba9",
-  "4e4990d3-217f-40f5-942b-c2dfe6d417b5",
+  "413c5c99-60d3-40e8-9fa8-4f8c47b0724c",
   "d7c2d6b4-aa3b-429a-a4c2-6cbe85297d0b",
 ];
 
