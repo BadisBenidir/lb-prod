b4009c1915c0a0020d853d7bc2e92223e1f11aac
BadisBenidir <107676252+BadisBenidir@users.noreply.github.com>
2026-01-26 00:24:36 +0000

    Modifications 22/01/2026 V1

--- src/pages/ProfileCompletionPage/ProfileCompletionPage.tsx
+++ src/pages/ProfileCompletionPage/ProfileCompletionPage.tsx
@@ -82,7 +82,7 @@ const ProfileCompletionPage: React.FC<ProfileCompletionPageProps> = ({
                 <User className="w-5 h-5 mr-3" />
                 Informations supplémentaires
               </h2>
-              <div className="grid grid-cols-2 gap-6">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <div>
                   <label 
                     htmlFor="birthDate" 
