bd1890481b1977678c8375875bb25764091e6b09
Badis Benidir <badisbenidir.pro@gmail.com>
2026-01-30 02:56:16 +0100

    Modifications 29/01/2026 V1

--- src/components/Product/ProductDetailPage.tsx
+++ src/components/Product/ProductDetailPage.tsx
@@ -261,18 +261,6 @@ const ProductDetailPage: React.FC = () => {
                     <span className="text-gray-600">État</span>
                     <span className="font-medium">{translateCondition(product.condition)}</span>
                   </div>
-                  <div className="flex justify-between">
-                    <span className="text-gray-600">N° de série</span>
-                    <span className="font-medium">
-                      {product.serialNumber ? (
-                        <span className="font-mono text-xs">{product.serialNumber}</span>
-                      ) : (
-                        <em className="text-gray-400 font-normal">Non communiqué</em>
-                      )}
-                    </span>
-                  </div>
-                </div>
-                <div className="space-y-2">
                   {product.genre && (
                     <div className="flex justify-between">
                       <span className="text-gray-600">Genre</span>
@@ -293,6 +281,16 @@ const ProductDetailPage: React.FC = () => {
                       </span>
                     </div>
                   )}
+                  <div className="flex justify-between">
+                    <span className="text-gray-600">N° de série</span>
+                    <span className="font-medium">
+                      {product.serialNumber ? (
+                        <span className="font-mono text-xs">{product.serialNumber}</span>
+                      ) : (
+                        <em className="text-gray-400 font-normal">Non communiqué</em>
+                      )}
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
