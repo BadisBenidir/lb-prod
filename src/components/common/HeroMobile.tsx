981f7a05e84b4e23efc136995a1925f715ce669f
Badis Benidir <badisbenidir.pro@gmail.com>
2026-02-03 15:28:43 +0100

    Modifications 29/01/2026 V1

--- src/components/common/HeroMobile.tsx
+++ src/components/common/HeroMobile.tsx
@@ -11,7 +11,7 @@ const HeroMobile: React.FC<HeroMobileProps> = ({ onNavigate }) => {
       {/* Animation (fond) */}
         <div className="absolute inset-0">
           <video
-            className="h-full w-full object-cover"
+            className="h-full w-full object-cover object-right"
             autoPlay
             muted
             loop
