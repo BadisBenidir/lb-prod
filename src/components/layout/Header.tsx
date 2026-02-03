ad1ac20d2dae11c93061adaf6de4cafaf5571d1b
Badis Benidir <badisbenidir.pro@gmail.com>
2026-02-03 02:58:32 +0100

    Modifications 29/01/2026 V1

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -89,7 +89,7 @@ const Header: React.FC<HeaderProps> = ({ cartItemsCount, onCartClick, user, onLo
   return (
     <header
       className={`
-        fixed top-0 left-0 right-0 z-[55]
+        fixed top-0 left-0 right-0 z-[50]
         transition-colors duration-300
         ${
           isHome && overHero
