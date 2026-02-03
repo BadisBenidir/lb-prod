a824a594afe3e06ce5150a00bbbcd00887283916
Badis Benidir <badisbenidir.pro@gmail.com>
2026-02-03 02:52:55 +0100

    Modifications 29/01/2026 V1

--- src/components/common/HeroDesktop.tsx
+++ src/components/common/HeroDesktop.tsx
@@ -79,9 +79,6 @@ const HeroDesktop: React.FC<HeroDesktopProps> = ({ onNavigate }) => {
                 <p className="text-sm text-gray-600">authenticité garantie</p>
               </div>
             </div>
-            {/* Éléments décoratifs */}
-            <div className="absolute -top-4 -right-4 w-24 h-24 border-2 border-black-900 -z-10"></div>
-            <div className="absolute -bottom-4 -left-4 w-32 h-32 bg-black-900 -z-10"></div>
           </div>
         </div>
       </div>
