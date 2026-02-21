7b076496318865041c2488c453e77638ee8e81aa
Badis Benidir <badisbenidir.pro@gmail.com>
2026-02-21 17:25:23 +0100

    Modifications 29/01/2026 V1

--- src/components/Checkout/ShippingFormMobile.tsx
+++ src/components/Checkout/ShippingFormMobile.tsx
@@ -270,7 +270,7 @@ const ShippingFormMobile: React.FC<ShippingFormMobileProps> = ({
       </div>
 
       {/* Choix du type de livraison */}
-      <div className="space-y-3 mb-4">
+      <div className="space-y-3 mb-10">
         <label className="block text-xs font-medium text-gray-700">
           Mode de livraison <span className="text-red-500">*</span>
         </label>
