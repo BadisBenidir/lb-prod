bf96a777e17cc6d08c85de4769684db4a6f966b5
Badis Benidir <badisbenidir.pro@gmail.com>
2026-02-23 18:26:02 +0100

    Modifications 29/01/2026 V1

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -77,7 +77,7 @@ const Header: React.FC<HeaderProps> = ({ cartItemsCount, onCartClick, user, onLo
     const heroHeight = hero.offsetHeight;
 
     const onScroll = () => {
-      if (window.scrollY > 700) {
+      if (window.scrollY > 650) {
         setOverHero(false);
       } else {
         setOverHero(true);
