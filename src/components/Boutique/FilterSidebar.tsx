b3773c080a55262dff698ebd32bb7bac57dafc1a
Badis Benidir <badisbenidir.pro@gmail.com>
2026-02-23 18:05:23 +0100

    Modifications 29/01/2026 V1

--- src/components/Boutique/FilterSidebar.tsx
+++ src/components/Boutique/FilterSidebar.tsx
@@ -185,12 +185,12 @@ const FilterSidebar: React.FC<FilterSidebarProps> = ({
       {/* Mobile backdrop */}
       {isOpen && (
         <div 
-          className="fixed inset-0 bg-black bg-opacity-50 lg:hidden"
+          className="fixed inset-0 bg-black bg-opacity-50 z-[45] lg:hidden"
           onClick={onClose}
         />
       )}
       
-      <div className={`fixed lg:relative top-16 lg:top-0 left-0 h-[calc(100vh-4rem)] lg:h-full w-80 bg-white border-r border-gray-200 transform transition-transform duration-300 ${
+      <div className={`fixed lg:relative top-16 lg:top-0 left-0 h-[calc(100vh-4rem)] lg:h-full w-80 bg-white border-r border-gray-200 z-[40] transform transition-transform duration-300 ${
         isOpen ? 'translate-x-0' : '-translate-x-full lg:translate-x-0'
       }`}>
         <div className="p-4 md:p-6 h-full overflow-y-auto">
