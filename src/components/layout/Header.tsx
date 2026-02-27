a270d4f81b495c199fca47ac1896ecb467fd4c9d
Badis Benidir <badisbenidir.pro@gmail.com>
2026-02-27 18:18:28 +0100

    Modifications 29/01/2026 V1

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -121,7 +121,7 @@ const Header: React.FC<HeaderProps> = ({ cartItemsCount, onCartClick, user, onLo
               <img
                 src="logov4.PNG"
                 alt="Ozë Paris Logo" 
-                className="h-20 w-20 lg:h-24 lg:w-24 mr-5 object-contain"
+                className="h-18 w-18 lg:h-22 lg:w-22 mr-5 object-contain"
               />
               <span className="text-xl lg:text-2xl font-bold tracking-tight text-black">
 
