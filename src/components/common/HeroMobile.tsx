06c44e2cba30ac146ff77d6d626f6028bd2f2b55
Badis Benidir <badisbenidir.pro@gmail.com>
2026-02-27 20:35:20 +0100

    Modifications 29/01/2026 V1

--- src/components/common/HeroMobile.tsx
+++ src/components/common/HeroMobile.tsx
@@ -19,7 +19,7 @@ const HeroMobile: React.FC<HeroMobileProps> = ({ onNavigate }) => {
             playsInline
             preload="auto"
           >
-            <source src="animv2.mp4" type="video/mp4" />
+            <source src="animv3.mp4" type="video/mp4" />
           </video>
         </div>
 
