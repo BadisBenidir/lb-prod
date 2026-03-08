de371d9cfe544b9742674160d197dd5f20a6311f
Badis Benidir <badisbenidir.pro@gmail.com>
2026-03-08 17:10:36 +0100

    Modifications 29/01/2026 V1

--- src/components/Checkout/ShippingForm.tsx
+++ src/components/Checkout/ShippingForm.tsx
@@ -22,7 +22,7 @@ const extractComponent = (components: any[], type: string) =>
 const ShippingForm: React.FC<ShippingFormProps> = ({ initialData, isUserConnected = false, onSubmit }) => {
   const [formData, setFormData] = useState<ShippingAddress>(initialData);
   const [errors, setErrors] = useState<Partial<ShippingAddress>>({});
-  const [deliveryType, setDeliveryType] = useState<DeliveryType>('point_relais');
+  const [deliveryType, setDeliveryType] = useState<DeliveryType>('domicile');
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
     const { name, value } = e.target;
