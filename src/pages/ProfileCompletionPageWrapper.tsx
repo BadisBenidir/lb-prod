e1584c66a1d5d172be6f865ed79f7fc32dee493b
Badis Benidir <badisbenidir.pro@gmail.com>
2026-03-12 15:12:46 +0100

    Mise à jour sécurisée

--- src/pages/ProfileCompletionPageWrapper.tsx
+++ src/pages/ProfileCompletionPageWrapper.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../../hooks/useAuth';
-import { supabase } from '../../lib/supabase';
-import ProfileCompletionPage from './ProfileCompletionPage';
-import { ProfileCompletionData } from '../../types/profile';
+import { useAuth } from '../hooks/useAuth';
+import { supabase } from '../lib/supabase';
+import { ProfileCompletionPage } from './ProfileCompletionPage'; // Ajoute les { } ici
+import { ProfileCompletionData } from '../types/profile';
 
 const ProfileCompletionPageWrapper: React.FC<{}> = () => {
   const navigate = useNavigate();
