8f67e71ed9e8f7b50b4c816507ee25ba6f32dc42
Badis Benidir <badisbenidir.pro@gmail.com>
2026-03-12 14:02:14 +0100

    Mise à jour sécurisée

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -418,7 +418,7 @@ function AppRouter() {
                     <Helmet>
                       <title>Compléter mon Profil - Ozë Paris</title>
                     </Helmet>
-                    <ProfileCompletionPage onComplete={async () => navigate('/')} />
+                    <ProfileCompletionPage />
                   </Layout>
                 </ProtectedRoute>
               } 
